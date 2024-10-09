65d21eab75acf41a4785cbca5cb766328f9829f8
arunshenoy99 <devarunshenoy99@gmail.com>
2024-10-09 11:42:12 +0530

    Update API integration and remove encryption logic

--- includes/Utilities/PLSUtility.php
+++ includes/Utilities/PLSUtility.php
@@ -2,8 +2,6 @@
 
 namespace NewfoldLabs\WP\Module\PLS\Utilities;
 
-use NewfoldLabs\WP\Module\Data\Helpers\Encryption;
-
 /**
  * Class PLSUtility
  *
@@ -13,62 +11,51 @@ class PLSUtility {
 
 	/**
 	 * Provisions a new license via the Hiive PLS API using the plugin slug.
-	 * If the license is already stored (encrypted) in WordPress options, it returns the stored license.
+	 * If the license is already stored in the WordPress option, it returns the stored license.
 	 *
 	 * @param string $plugin_slug The plugin slug.
 	 *
 	 * @return array|WP_Error License data or WP_Error on failure.
 	 */
 	public static function provision_license( $plugin_slug ) {
-		$encryption = new Encryption();
+		// Retrieve the stored license data from the WordPress option.
+		$option_name  = 'nfd_module_pls_license_data_' . $plugin_slug;
+		$license_data = get_option( $option_name );
 
-		$option_name       = 'nfd_module_pls_license_data_' . $plugin_slug;
-		$encrypted_license = get_option( $option_name );
-		// Check if the license already exists in WordPress options
-		if ( $encrypted_license ) {
-			$decrypted_license = $encryption->decrypt( $encrypted_license );
-			if ( $decrypted_license ) {
-				// TODO: Validate the license's validity (if necessary) once the Hiive API is ready.
-				return json_decode( $decrypted_license, true );
-			}
-
-			delete_option( $option_name );
-			return new \WP_Error(
-				'nfd_pls_error',
-				__( 'Failed to decrypt the stored license.', 'wp-module-pls' )
-			);
+		// Check if the license data already exists in the options.
+		// TODO: Update this to store only the license ID in the appropriate plugin option once the Hiive API is ready and can store the download URL as well.
+		if ( $license_data ) {
+			return json_decode( $license_data, true );
 		}
 
-		// License doesn't exist, so proceed with the API call to provision a new one
-		$endpoint      = '/sites/v2/pls';
-		$body          = array(
+		// If no license is found, proceed with an API request to provision a new license.
+		$endpoint = '/sites/v2/pls/license';
+		$body     = array(
 			'pluginSlug' => $plugin_slug,
 		);
-		$hiive_request = new HiiveUtility( $endpoint, $body, 'POST' );
 
-		$response = $hiive_request->send_request();
+		$hiive_request = new HiiveUtility( $endpoint, $body, 'POST' );
+		$response      = $hiive_request->send_request();
 		if ( is_wp_error( $response ) ) {
 			return $response;
 		}
 
 		$response_body = json_decode( $response, true );
+		// Check if the response contains the necessary license ID and download URL.
 		if ( isset( $response_body['license_id'], $response_body['download_url'] ) ) {
-			$license_data   = array(
+			$license_data = array(
 				'licenseId'     => $response_body['license_id'],
 				'downloadUrl'   => $response_body['download_url'],
-				'storageMethod' => 'wp_options',
+				'storageMethod' => 'wp_option',
 				'storageKey'    => $option_name,
 			);
-			$encrypted_data = $encryption->encrypt( wp_json_encode( $license_data ) );
-			if ( $encrypted_data ) {
-				update_option( $option_name, $encrypted_data );
-			}
+
+			update_option( $option_name, wp_json_encode( $license_data ) );
 
 			return $license_data;
 		}
 
-		// If the response format is unexpected, return an error
-		return new WP_Error(
+		return new \WP_Error(
 			'nfd_pls_error',
 			__( 'Unexpected response format from the API.', 'wp-module-pls' )
 		);
